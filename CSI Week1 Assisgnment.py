eb280e838931179ec1f866d07a65a03fe24cd87d
Aryan Roy <144680046+A-r-y-a-n-R-o-y@users.noreply.github.com>
2025-06-08 11:24:11 +0530

    Update CSI Week1 Assisgnment.py

--- CSI Week1 Assisgnment.py
+++ CSI Week1 Assisgnment.py
@@ -2,21 +2,30 @@
 #Lower Triangle
 rows = int(input("Enter number of rows: "))
 
-for i in range(rows):
-    for j in range(i+1):
+for a in range(rows):
+    for b in range(a+1):
         print("* ", end="")
     print()
 
 #Upper Triangle
 rows = int(input("Enter number of rows: "))
 
-for i in range(rows, 0 ,-1):
-    for j in range(0,i):
+for c in range(rows, 0 ,-1):
+    for d in range(0,c):
         print("* ", end=" ")
     print()
 
 #Pyramid
 rows = int(input("Enter number of rows: "))
+e = 0
 
-for i in range(rows):
-    print(" " * (rows - i - 1) + "* " * (i + 1))
+for f in range(1, rows+1):
+    for space in range(1, (rows-f)+1):
+        print(end="  ")
+   
+    while e!=(2*f-1):
+        print("* ", end="")
+        e += 1
+   
+    e = 0
+    print()
